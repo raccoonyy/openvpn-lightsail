c70dfee78787036f8aa9970281c09799f2adaebf
raccoony <raccoonyy@gmail.com>
2019-01-11 10:45:31 +0900

    Revert "Sudo for generate profile file"
    This reverts commit 8d3fd729b90c70823258ae67e9f81476c7a1a41c.

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -33,16 +33,16 @@ newclient () {
 	# Generates the custom client.ovpn
 	cp /etc/openvpn/client-common.txt ~/$1.ovpn
 	echo "<ca>" >> ~/$1.ovpn
-	sudo cat /etc/openvpn/easy-rsa/pki/ca.crt >> ~/$1.ovpn
+	cat /etc/openvpn/easy-rsa/pki/ca.crt >> ~/$1.ovpn
 	echo "</ca>" >> ~/$1.ovpn
 	echo "<cert>" >> ~/$1.ovpn
-	sudo cat /etc/openvpn/easy-rsa/pki/issued/$1.crt >> ~/$1.ovpn
+	cat /etc/openvpn/easy-rsa/pki/issued/$1.crt >> ~/$1.ovpn
 	echo "</cert>" >> ~/$1.ovpn
 	echo "<key>" >> ~/$1.ovpn
-	sudo cat /etc/openvpn/easy-rsa/pki/private/$1.key >> ~/$1.ovpn
+	cat /etc/openvpn/easy-rsa/pki/private/$1.key >> ~/$1.ovpn
 	echo "</key>" >> ~/$1.ovpn
 	echo "<tls-auth>" >> ~/$1.ovpn
-	sudo cat /etc/openvpn/ta.key >> ~/$1.ovpn
+	cat /etc/openvpn/ta.key >> ~/$1.ovpn
 	echo "</tls-auth>" >> ~/$1.ovpn
 }
 
